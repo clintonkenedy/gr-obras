998dc3acaebf6aa4aac2dbac02b668088909096b
Cesar-CCC <cusijustoc@gmail.com>
2023-07-14 16:49:01 -0500

    modificado service de profesiones

--- front-gr-obras/src/services/ProfesionesService.js
+++ front-gr-obras/src/services/ProfesionesService.js
@@ -1,23 +1,24 @@
 import axios from "axios";
+import http from "src/utils/http";
 
 class ProfesionesService {
     static async getData() {
-        return (await axios.get('http://localhost:8000/api/obras/profesion')).data;
+        return (await html.get('/api/obras/profesion')).data;
     }
 
     static async get(id) {
-        return (await axios.get(`http://localhost:8000/api/obras/profesion/${id}`)).data;
+        return (await html.get(`/api/obras/profesion/${id}`)).data;
     }
 
     static async delete(id) {
-        return (await axios.delete(`http://localhost:8000/api/obras/profesion/${id}`));
+        return (await html.delete(`/api/obras/profesion/${id}`));
     }
 
     static async save(reg) {
         if (reg.id === undefined || reg.id === null) {
-            return (await axios.post("http://localhost:8000/api/obras/profesion", reg)).data;
+            return (await html.post("/api/obras/profesion", reg)).data;
         } else {
-            return (await axios.put(`http://localhost:8000/api/obras/profesion/${reg.id}`, reg)).data;
+            return (await html.put(`/api/obras/profesion/${reg.id}`, reg)).data;
         }
     }
 }
