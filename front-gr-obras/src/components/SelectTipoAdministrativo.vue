741156b1ca6d6aac3660db66588db582b7ca8c1c
DLeonelCC <drakeleonelc@gmail.com>
2023-09-03 21:15:09 -0500

    Avance Dashboard

---
+++ front-gr-obras/src/components/SelectTipoAdministrativo.vue
@@ -0,0 +1,12 @@
+<template>
+    <q-select borderless dense debounce="300" outlined :options="options" label="Tipo A." />
+</template>
+
+<script setup>
+const options = [
+    'COORDINADOR',
+    'SUPERVISOR',
+    'RESIDENTE',
+    'ECONOMISTA'
+]
+</script>
